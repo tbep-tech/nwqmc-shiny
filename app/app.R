14c7c07f58d1f9a42617b498facda60372de150a
fawda123 <mbafs2012@gmail.com>
2025-01-20 10:47:18 -0500

    more content to fundamentals

---
+++ app/app.R
@@ -0,0 +1,38 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("My awesome histogram app"),
+  
+  # Sidebar with a numeric input for sample size 
+  sidebarLayout(
+    sidebarPanel(
+      numericInput(inputId = 'n', label = 'Sample size', value = 50), 
+      selectInput(inputId = 'col', label = 'Choose color', choices = c('red', 'blue', 'green'))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    
+    # generate random sample of size n
+    x <- rnorm(input$n)
+    
+    # draw the histogram
+    hist(x, col = input$col)
+    
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
